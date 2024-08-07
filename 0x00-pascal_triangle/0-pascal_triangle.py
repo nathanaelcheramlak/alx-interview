e22c9d12c45c847dab7c5aae61df0a87efc336c1
Nathanael Cheramlak <nathanaelcheramlak7@gmail.com>
2024-08-08 01:16:15 +0300

    pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,19 @@
+def calcFactorial(num):
+    if num <= 0:
+        return 1
+    return num * calcFactorial(num - 1)
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    for line in range(n):
+        container = list()
+        lineFactorial = calcFactorial(line)
+        for item in range(line + 1):
+            itemFactorial = calcFactorial(item)
+            combinedFactorial = calcFactorial(line - item)
+
+            final = int(lineFactorial / (itemFactorial * combinedFactorial))
+            container.append(final)
+        
+        print(container)
