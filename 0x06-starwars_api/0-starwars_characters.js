303eb2e577817d51f89e40f0459809368032a1ad
Nathanael Cheramlak <nathanaelcheramlak7@gmail.com>
2024-09-26 14:41:24 +0300

    starwars api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,37 +1,27 @@
 #!/usr/bin/node
-
 const request = require("request");
-const movieId = process.argv[2];
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+const API_URL = "https://swapi-api.hbtn.io/api";
 
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error(error);
-    return;
-  }
-
-  const filmData = JSON.parse(body);
-  const characters = filmData.characters;
-
-  // Fetch each character's name in order using a loop
-  printCharacters(characters, 0);
-});
-
-function printCharacters(characters, index) {
-  if (index >= characters.length) {
-    return; // End of the list, stop recursion
-  }
-
-  request(characters[index], (charError, charResponse, charBody) => {
-    if (charError) {
-      console.error(charError);
-      return;
+if (process.argv.length > 2) {
+  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
+    if (err) {
+      console.log(err);
     }
+    const charactersURL = JSON.parse(body).characters;
+    const charactersName = charactersURL.map(
+      (url) =>
+        new Promise((resolve, reject) => {
+          request(url, (promiseErr, __, charactersReqBody) => {
+            if (promiseErr) {
+              reject(promiseErr);
+            }
+            resolve(JSON.parse(charactersReqBody).name);
+          });
+        })
+    );
 
-    const characterData = JSON.parse(charBody);
-    console.log(characterData.name);
-
-    // Call the function recursively for the next character
-    printCharacters(characters, index + 1);
+    Promise.all(charactersName)
+      .then((names) => console.log(names.join("\n")))
+      .catch((allErr) => console.log(allErr));
   });
 }
